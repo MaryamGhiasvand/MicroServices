b66aecd4611dac8a491fdd9063d49b6b39a18929
MaryamGhiasvand <m.ghiyasvand001@gmail.com>
2020-09-27 00:17:04 -0500

    create restTemplate in bean => we have singleton RT for every rest api call

--- movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/MovieCatalogServiceApplication.java
+++ movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -2,6 +2,8 @@ package io.javabrains.moviecatalogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class MovieCatalogServiceApplication {
@@ -10,4 +12,10 @@ public class MovieCatalogServiceApplication {
 		SpringApplication.run(MovieCatalogServiceApplication.class, args);
 	}
 
+	//since beans are singleton by default => it returns exactly one rest template for the whole app
+	//this method executes just once => if any api needs it, it returns exactly the same one
+	@Bean
+	public RestTemplate getRrRestTemplate(){
+		return new RestTemplate();
+	}
 }
--- movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/resources/MovieCatalogResource.java
@@ -3,6 +3,7 @@ package io.javabrains.moviecatalogservice.resources;
 import io.javabrains.moviecatalogservice.models.CatalogItem;
 import io.javabrains.moviecatalogservice.models.Movie;
 import io.javabrains.moviecatalogservice.models.Rating;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,10 +15,16 @@ import java.util.stream.Collectors;
 @RestController
 @RequestMapping("/catalog")
 public class MovieCatalogResource {
+    @Autowired //is by type => somewhere there is a bean of type RestTemplate , inject it here
+    //if there is more than 1 bean with the same type, we should tag them with qualifier
+    //both bean and Autowired
+    private RestTemplate restTemplate; //create a property of class of the same type of Bean
+
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
         //create an instance of rest template ( a utility object that is supposed to make rest api call)
-        RestTemplate restTemplate = new RestTemplate();
+        //RestTemplate restTemplate = new RestTemplate();
+
         //1.get all the rated movie id
         List<Rating> ratings = Arrays.asList(
                 new Rating("movie1", 4),
@@ -25,24 +32,17 @@ public class MovieCatalogResource {
         );
 
         //2."for each movie" id "call info service" and get detail
-        ratings
+        return ratings
                 .stream()
                 //for each movie make a separate call and return the "movie"
                 //these call are sync => to be async(at the same time) => use webClient
                 .map(rating -> {
-                    //"option"+"cmd"+"v" => to create a variable name for it 
+                    //"option"+"cmd"+"v" => to create a variable name for it
                     Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
 
                     //3. put them all together
                     return new CatalogItem(movie.getMovieName(),"desc",rating.getRating());
                 })
                 .collect(Collectors.toList());
-
-
-
-        return Collections.singletonList(
-                //new CatalogItem("Spiderman","desc Spiderman",4),
-                new CatalogItem("inception", "dec",5)
-        );
     }
 }
