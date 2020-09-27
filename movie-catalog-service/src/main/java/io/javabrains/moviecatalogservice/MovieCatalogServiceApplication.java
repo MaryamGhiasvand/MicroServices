0ad75b3769af1fbc1b681e6b9072d935330b1fa8
MaryamGhiasvand <m.ghiyasvand001@gmail.com>
2020-09-27 13:36:15 -0500

    Call webApi by WebClient instead of RestTemplate

--- movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/MovieCatalogServiceApplication.java
+++ movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class MovieCatalogServiceApplication {
@@ -14,8 +15,15 @@ public class MovieCatalogServiceApplication {
 
 	//since beans are singleton by default => it returns exactly one rest template for the whole app
 	//this method executes just once => if any api needs it, it returns exactly the same one
+	//same as a builder
 	@Bean
 	public RestTemplate getRrRestTemplate(){
 		return new RestTemplate();
+	}
+
+	//use weblient instead of RestTemplate
+	@Bean
+	public WebClient.Builder getWebClientBuilder(){
+		return WebClient.builder();
 	}
 }
--- movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/resources/MovieCatalogResource.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.*;
 import java.util.stream.Collectors;
@@ -19,6 +20,10 @@ public class MovieCatalogResource {
     //if there is more than 1 bean with the same type, we should tag them with qualifier
     //both bean and Autowired
     private RestTemplate restTemplate; //create a property of class of the same type of Bean
+
+    @Autowired
+    private WebClient.Builder webClientBuilder;
+
 
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
@@ -40,8 +45,21 @@ public class MovieCatalogResource {
                     //"option"+"cmd"+"v" => to create a variable name for it
                     Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
 
+                    //TO GET DATA FROM WEB CLIENT:
+                    //Is a REACTIVE STREAM
+                    Movie movieFromWebClient = webClientBuilder.build()
+                            .get() //type of method
+                            .uri("http://localhost:8082/movies/" + rating.getMovieId()) //the path of service
+                            .retrieve() //fetch this data
+                            .bodyToMono(Movie.class) //convert the body to this instance of this class
+                            //mono in async way is same as a proxy
+                            .block(); //blocks the execution until the result gets back and mono is fulfilled
+
+
                     //3. put them all together
+                    //instead of movie can be "movieFromWebClient"
                     return new CatalogItem(movie.getMovieName(),"desc",rating.getRating());
+
                 })
                 .collect(Collectors.toList());
     }
