cc90ea428cd207452cc58e9d142d214eaba3f637
MaryamGhiasvand <m.ghiyasvand001@gmail.com>
2020-09-27 22:11:04 -0500

    Add Eureka client to movie-info

--- movie-info-service/src/main/java/io/javabrains/movieinfoservice/MovieInfoServiceApplication.java
+++ movie-info-service/src/main/java/io/javabrains/movieinfoservice/MovieInfoServiceApplication.java
@@ -2,8 +2,11 @@ package io.javabrains.movieinfoservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+//for previous versions was mandatory, but now it is not (just to make it explicit)
+@EnableEurekaClient
 public class MovieInfoServiceApplication {
 
 	public static void main(String[] args) {
